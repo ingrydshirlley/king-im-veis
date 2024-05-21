6b22879668a901a1e9e46c748d2bc7f7923cc48f
ingryd <ingryddasilva7@gmail.com>
2024-05-21 13:25:30 -0300

    casa jd sol nascente

--- js/botoes.js
+++ js/botoes.js
@@ -1,8 +1,19 @@
 const topoButton = document.getElementById('topo');
 
+window.addEventListener('scroll', () => {
+    if (window.scrollY > 300) { // Mostra o botão após rolar 300px para baixo
+        topoButton.style.display = 'flex';
+    } else {
+        topoButton.style.display = 'none';
+    }
+});
+
 topoButton.addEventListener('click', () => {
     window.scrollTo({ 
         top: 0, behavior: 'smooth' 
     });
 });
+
+
+
 
